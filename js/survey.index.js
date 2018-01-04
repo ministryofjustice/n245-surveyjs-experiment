45d59704ff2747b0670fb4978c5875d51cfa818e
Alex Robinson <github@alex.fu2k.org>
2018-01-04 23:04:03 +0000

    Use correct way of adding class names to elements

--- js/survey.index.js
+++ js/survey.index.js
@@ -12,41 +12,22 @@ jQuery(() => {
     logger('handleLoadSurvey')
   }
 
-  const classMap = {
-    sv_page_title: 'heading-large',
-    sv_qstn: 'form-group',
-    sv_q_title: 'form-label-bold',
-    sv_q_text_root: 'form-control',
-    sv_q_dropdown_control: 'form-control',
-    'sv_qstn textarea': 'form-control',
-    sv_q_erbox: 'error-message',
-    sv_next_btn: 'button'
-  }
-
-  const updateElements = e => {
-    Object.keys(classMap).forEach(classKey => {
-      jQuery(`.${classKey}`).addClass(classMap[classKey])
-    })
-    
-    return
-
-    // add multiple-choice class to checkbox elements
-
-    // move the input to before the label. this makes the checkboxes
-    // look correct, but has some problems;
-    //   - When the user checks the box, it reverts to the default
-    //     appearance, and stays that way even if unchecked
-    //   - The user has to click on the checkbox itself - clicking on
-    //     the label does nothing
-
-  }
-
   const handleAfterRenderPage = () => {
     logger('handleAfterRenderPage')
   }
 
   const handleCurrentPageChanged = () => {
     logger('handleCurrentPageChanged')
+  }
+
+  const classMap = {
+    'sv_qstn textarea': 'form-control'
+  }
+
+  const updateElements = e => {
+    Object.keys(classMap).forEach(classKey => {
+      jQuery(`.${classKey}`).addClass(classMap[classKey])
+    })
   }
 
   const handleQuestionRender = (e, question) => {
@@ -57,24 +38,20 @@ jQuery(() => {
     $question[classMethod]('form-control-optional')
     updateElements()
     // but it doesn't fire when errors change
-    // const hasErrors = !!jQuery('.error-message', $question).length
-    // const classMethod = hasErrors ? 'addClass' : 'removeClass'
-    // $question[classMethod]('form-group-error')
+    // so error updating on enclosing form-control element is in the setInterval method
   }
 
-  const survey = new Survey.Model(surveyJSON);
-  survey.registerFunctionOnPropertyValueChanged((...args) => {
-    console.log(args)
-  })
+  const survey = new Survey.Model(surveyJSON)
 
   jQuery("#surveyContainer").Survey({
     model: survey,
+    css: surveyJSON.cssClasses,
     onComplete: sendDataToServer,
     onAfterRenderPage: handleAfterRenderPage,
     onCurrentPageChanged: handleCurrentPageChanged,
     onAfterRenderSurvey: handleLoadSurvey,
     onAfterRenderQuestion: handleQuestionRender
-  });
+  })
 
   const $body = jQuery('body')
   const setBackLink = () => {
@@ -83,9 +60,8 @@ jQuery(() => {
   }
 
   const updateMultipleChoice = () => {
-    jQuery('.sv_q_checkbox, .sv_q_radiogroup').addClass('multiple-choice')
     const $multichoice = jQuery('.multiple-choice')
-    $multichoice.each(function() {
+    $multichoice.each(function () {
       const lbl = jQuery(this).find('label').get(0)
       const inp = jQuery(lbl).find('input').get(0)
       if (inp) {
@@ -97,20 +73,19 @@ jQuery(() => {
         jQuery(lbl).before(inp)
       }
     })
-  } 
+  }
+
+  const updateControlErrors = () => {
+    jQuery('.form-group').each(function () {
+      const $formGroup = jQuery(this)
+      const hasErrors = !!jQuery('.error-message', $formGroup).length
+      const classMethod = hasErrors ? 'addClass' : 'removeClass'
+      $formGroup[classMethod]('form-group-error')
+    })
+  }
 
   setInterval(() => {
-    // if (survey.isCurrentPageHasErrors) {
-      jQuery('.sv_q_erbox').addClass('error-message')
-      //jQuery('.sv_qstn').addClass('form-group')
-      // jQuery('.sv_next_btn').addClass('button')
-      jQuery('.form-group').each(function() {
-        const $formGroup = jQuery(this)
-        const hasErrors = !!jQuery('.error-message', $formGroup).length
-        const classMethod = hasErrors ? 'addClass' : 'removeClass'
-        $formGroup[classMethod]('form-group-error')
-      })
-    // }
+    updateControlErrors()
     setBackLink()
     updateMultipleChoice()
   }, 10)
--- js/survey.json.defaults.js
+++ js/survey.json.defaults.js
@@ -2,5 +2,34 @@ var surveyJSONDefaults = {
   showQuestionNumbers: "off",
   pagePrevText: "Back",
   pageNextText: "Continue",
-  requiredText: ""
+  startSurveyText: "Start right now",
+  requiredText: "",
+  cssClasses: {
+    navigation: {
+      next: "sv_next_btn button",
+      start: "sv_start_btn"
+    },
+
+    pageTitle: "sv_page_title heading-large",
+    question: {
+        mainRoot: "sv_q sv_qstn form-group",
+        title: "sv_q_title form-label-bold"
+    },
+    error: {
+      root: "sv_q_erbox error-message"
+    },
+    checkbox: {
+        item: "sv_q_checkbox multiple-choice"
+    },
+    dropdown: {
+        control: "sv_q_dropdown_control form-control"
+    },
+    matrixdynamic: {
+        button: "sv_matrix_dynamic_button"
+    },
+    radiogroup: {
+        item: "sv_q_radiogroup multiple-choice"
+    },
+    text: "sv_q_text_root form-control"
+  }
 }
